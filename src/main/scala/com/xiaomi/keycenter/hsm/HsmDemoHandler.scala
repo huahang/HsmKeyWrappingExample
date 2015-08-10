d50eed9ecc91eb0cf05c2e42e16480923270629b
liuhuahang <liuhuahang@zerus.co>
2015-08-10 11:58:02 +0800

    s
    Signed-off-by: liuhuahang <liuhuahang@zerus.co>

--- src/main/scala/com/xiaomi/keycenter/hsm/HsmDemoHandler.scala
+++ src/main/scala/com/xiaomi/keycenter/hsm/HsmDemoHandler.scala
@@ -155,19 +155,19 @@ class HsmDemoHandler extends HttpServiceActor {
         val keyGenerator = KeyGenerator.getInstance("AES", "SunJCE")
         keyGenerator.init(256, new SecureRandom)
         val secretKey = keyGenerator.generateKey()
-//        val encryptCipher = Cipher.getInstance("AES/GCM/NoPadding", "BC")
-//        encryptCipher.init(Cipher.ENCRYPT_MODE, secretKey, new IvParameterSpec("0102030405060708".getBytes))
-//        val cipher = encryptCipher.doFinal(data)
+        val encryptCipher = Cipher.getInstance("AES/GCM/NoPadding", "BC")
+        encryptCipher.init(Cipher.ENCRYPT_MODE, secretKey, new IvParameterSpec("0102030405060708".getBytes))
+        val cipher = encryptCipher.doFinal(data)
         val keyCipher = service.wrap("666_kek", secretKey)
         val unwrappedKey = service.unwrap("666_kek", keyCipher, secretKey.getAlgorithm, Cipher.SECRET_KEY)
-//        val decryptCipher = Cipher.getInstance("AES/GCM/NoPadding", "LunaProvider")
-//        decryptCipher.init(Cipher.DECRYPT_MODE, unwrappedKey, new IvParameterSpec("0102030405060708".getBytes))
-//        val dataString = new String(decryptCipher.doFinal(cipher), Charsets.UTF_8)
+        val decryptCipher = Cipher.getInstance("AES/GCM/NoPadding", "LunaProvider")
+        decryptCipher.init(Cipher.DECRYPT_MODE, unwrappedKey, new IvParameterSpec("0102030405060708".getBytes))
+        val dataString = new String(decryptCipher.doFinal(cipher), Charsets.UTF_8)
 
         ctx.complete(
           "ok" + "\r\n" +
             "key generator provider: " + keyGenerator.getProvider.getClass.getCanonicalName + "\r\n" +
-//            "data string: " + dataString + "\r\n" +
+            "data string: " + dataString + "\r\n" +
             "secret key:" + "\r\n" + key2string(secretKey) + "\r\n" +
             "unwrapped key:" + "\r\n" + key2string(unwrappedKey) + "\r\n"
         )
